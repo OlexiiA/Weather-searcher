16a686ff29592afdcaeb99ba57caadd49b319865
OlexiiA <incretiblealex@gmail.com>
2022-11-20 22:27:08 +0200

    Ukrain

--- src/index.js
+++ src/index.js
@@ -15,25 +15,28 @@ divRef.innerHTML = ""; // при сабміті очищує контент;
 }
 
 
-function getData(data, value) {
+function getData(data) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=f1462fa9372c813fa8285011e884b6ef&units=metric&lang=ua`)
     .then(responce => responce.json())
-    .then(data => addMarkup(data))
+    .then(res => addMarkup(res, data))
 }
 
 
-function addMarkup(data) {
+function addMarkup(data, value) {
     const temperature = Math.round(data.main.temp);
     const feels_like = Math.round(data.main.feels_like) 
+    
     const template =  `<div class="weather__header">
     <div class="weather__main">
-        <div class="weather__city">${data.name}</div>
+        <div class="weather__city">${value}</div>
         <div class="weather__icon"><img src='http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png' alt='' width='150' height='150'></div>
         
     </div>
-    <div class="weather__status"><div >Weather status:<span class="span">${data.weather[0].main}</span></div>
-    <div class="weather__temp">Тemperature: <span class="span">${temperature} С°</span></div>
-    <div class="weather__feels-likes">Fells likes: <span class="span">${feels_like} С°</span></div></div>
+    <div class="weather__status"><div >Стан погоди:<span class="span">${data.weather[0].description}</span></div>
+    <div class="weather__temp">Температура повітря: <span class="span">${temperature} С°</span></div>
+    <div class="weather__feels-likes">Відчувається як: <span class="span">${feels_like} С°</span></div></div>
     </div>`
     divRef.insertAdjacentHTML(`beforeend`, template);
+    
+    
 }
